a9e576b927e21fc00efbaea52bfd9bf3c6c06cb5
Killian Varescon <varesconkillian@gmail.com>
2024-08-10 22:10:04 +0200

    switch from setup (setuptools) to pyproject.toml file

--- setup.py
+++ setup.py
@@ -12,69 +12,14 @@ def read(*names, **kwargs):
 
 
 setup(
-    name="syml",
-    version="0.2.0",
-    license="MIT",
-    description="SyML (Systematic Machine Learning) is library built to make Machine Learning simpler, by using SOTA ML, xAI and vizualisation methods.",
     long_description="{}\n{}".format(
         re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub("", read("README.rst")),
         re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author="Killian Varescon",
-    author_email="varesconk@gmail.com",
     url="https://github.com/KillianVar/python-syml",
     packages=find_packages("src"),
     package_dir={"": "src"},
     py_modules=[path.stem for path in Path("src").glob("*.py")],
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        # uncomment if you test on these interpreters:
-        # "Programming Language :: Python :: Implementation :: IronPython",
-        # "Programming Language :: Python :: Implementation :: Jython",
-        # "Programming Language :: Python :: Implementation :: Stackless",
-        "Topic :: Utilities",
-    ],
-    project_urls={
-        "Documentation": "https://python-syml.readthedocs.io/",
-        "Changelog": "https://python-syml.readthedocs.io/en/latest/changelog.html",
-        "Issue Tracker": "https://github.com/KillianVar/python-syml/issues",
-    },
-    keywords=[
-        "ML",
-        "machine-learning",
-        "xAI",
-        # eg: "keyword1", "keyword2", "keyword3",
-    ],
-    python_requires=">=3.8",
-    install_requires=[
-        # eg: "aspectlib==1.1.1", "six>=1.7",
-    ],
-    extras_require={
-        # eg:
-        #   "rst": ["docutils>=0.11"],
-        #   ":python_version=='3.8'": ["backports.zoneinfo"],
-    },
-    entry_points={
-        "console_scripts": [
-            "syml = syml.cli:run",
-        ]
-    },
 )
