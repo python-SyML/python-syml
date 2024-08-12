fee32f2a91d960d3f1269d3da25793c04658b642
Killian Varescon <varesconkillian@gmail.com>
2024-08-12 19:07:45 +0200

    added test files and patched minor errors.

--- src/syml/core.py
+++ src/syml/core.py
@@ -3,11 +3,6 @@ from sklearn.datasets import load_iris
 
 from syml.interract.dashboard import Dashboard
 
-
-def compute(args):
-    return max(args, key=len)
-
-
 if __name__ == "__main__":
     data = load_iris()
     df = pd.DataFrame(data=data.data, columns=data.feature_names)
