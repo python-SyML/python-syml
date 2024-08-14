0936cb8fa9de43a37dbebaec9f9cb42db0f5b283
Killian Varescon <varesconkillian@gmail.com>
2024-08-14 22:07:21 +0200

    Bump version: 0.3.11 → 0.3.12

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "SyML"
 year = "2024"
 author = "Killian Varescon"
 copyright = f"{year}, {author}"
-version = release = "0.3.11"
+version = release = "0.3.12"
 
 pygments_style = "trac"
 templates_path = ["."]
